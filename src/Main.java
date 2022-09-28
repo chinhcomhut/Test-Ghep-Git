157ec39fb3946625e30293917af7bdb25e026029
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2022-09-29 02:06:02 +0700

    dev lan 3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("lan 1");
         System.out.println("lan 2");
+        System.out.println("dev lan 3");
     }
 }
